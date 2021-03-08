a474cd0820b577bdb0e8690e02ded8405b85ba54
maxagon <s.shadrov98@gmail.com>
2021-03-08 10:07:46 +0300

    Better debugging

--- Sources/Utils/DebugCheck.h
+++ Sources/Utils/DebugCheck.h
@@ -1,12 +1,33 @@
 #pragma once
 
-#include <stdexcept>
-#include "winerror.h"
+#include <windows.h>
+#include <intrin.h>
+
+static bool ShowDebugMessageBox(const char* message)
+{
+    int msgboxID = MessageBox(NULL, (LPCSTR)message, (LPCSTR)"Debug message", MB_ICONWARNING | MB_ABORTRETRYIGNORE | MB_DEFBUTTON2);
+
+    switch (msgboxID)
+    {
+    case IDABORT:
+        exit(1);
+        break;
+    case IDRETRY:
+        return true;
+        break;
+    case IDIGNORE:
+        break;
+    }
+    return false;
+}
 
 #define DCHECK(condition) \
 if (!(condition))\
 {\
-    throw std::runtime_error(#condition);\
+    if (ShowDebugMessageBox("Failed: \n" ## #condition))\
+    {\
+        __debugbreak();\
+    }\
 }
 
 #define DCHECK_COM(hr) DCHECK(SUCCEEDED(hr))
